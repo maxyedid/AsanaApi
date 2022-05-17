c86b56cb1753b914268ed4f9513882d69c3863a4
Max Yedid <maxyedid1@gmail.com>
2022-05-17 18:19:44 -0400

    Updating nnumber of recent tasks to update

--- Asana/app.js
+++ Asana/app.js
@@ -1,12 +1,12 @@
 const asana = require('asana')
-const config = require(".../../config")
+const config = require("./config")
 require('dotenv').config()
 
 const client = asana.Client.create().useAccessToken(process.env.TOKEN)
 
-const defaultLimit = 100;
+const defaultLimit = 30;
 
-let numTasks = 1;
+let numTasks = 50;
 
 const delay = 200
 //helper task
